6b409d6cdae329492ecea2d26ca3e2f9144024f7
henansong <github@nansenho.com>
2021-01-27 18:29:23 +0900

    引入CSS，JS

---
+++ public/demo.js
@@ -0,0 +1,4 @@
+console.log('This is demo.js imported')
+const div = document.createElement('div')
+div.innerHTML = 'This is demo.js imported'
+document.body.appendChild(div)
---
+++ public/main.js
@@ -0,0 +1,29 @@
+getCSS.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('GET', '/style.css')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const style = document.createElement('style')
+                style.innerHTML = request.response
+                document.head.appendChild(style)
+            }
+        }
+    }
+    request.send()
+}
+
+getJS.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET', '/demo.js')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const script = document.createElement('script')
+                script.innerHTML = request.response
+                document.body.appendChild(script)
+            }
+        }
+    }
+    request.send()
+}
