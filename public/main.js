863b218518e56a8580cf44b06bccbbe3cc1b036b
henansong <github@nansenho.com>
2021-01-29 11:14:48 +0800

    ajax 加载 json

--- public/main.js
+++ public/main.js
@@ -44,3 +44,20 @@ getXML.onclick = ()=>{
     xhr.send()
 }
 
+getJSON.onclick = ()=>{
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET','/demo.json')
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState === 4){
+            if(xhr.status >= 200 && xhr.status < 300){
+                console.log(xhr.response);
+                const object = JSON.parse(xhr.response)
+                myName.textContent = object.name
+            }
+        }
+    }
+    xhr.send()
+}
+
+
+
--- server.js
+++ server.js
@@ -46,6 +46,11 @@ var server = http.createServer(function(request, response){
         response.setHeader('Content-Type', 'text/xml; charset=utf-8')
         response.write(fs.readFileSync('public/demo.xml'))
         response.end()
+    } else if(path === '/demo.json'){
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'application/json; charset=utf-8')
+        response.write(fs.readFileSync('public/demo.json'))
+        response.end()
     } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
